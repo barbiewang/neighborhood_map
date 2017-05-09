561d6062a23a8fbfe9e7f4b240999aaac235306d
BarbieWang <736989974@qq.com>
2017-05-09 09:05:45 +0800

    delete unuseful codes

--- js/main.js
+++ js/main.js
@@ -20,41 +20,19 @@ MapViewModel.prototype.disableDetails = function(){
 MapViewModel.prototype.showMenu = function(){
     $(".menuUl").display = "block";
 };
-// MapViewModel.prototype.nytimeData = function(){
-//      for(var i = 0;i<this.placeInEnglish;i++){
-//          var place = this.placeInEnglish[i];
-//          var nytimeUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + place + '&sort=newest&api-key=ef6858b2678449d99891114791de52ba';
-//          $.getJson(nytimeUrl,function(data){
-//              var article = data.response.docs[0];
-//                  $("#nytime").append('<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
-//                      '<p>' + article.snippet + '</p>')
-//
-//          }).fail(function(){
-//              $("#nytime").text('New York Times Articles Could Not Be Loaded');
-//          })
-//      }
-// };
-// MapViewModel.prototype.toggleNytime = function(){
-//     var nytime = $("#nytime");
-//     if(nytime.display !== null){
-//         nytime.display = "block"
-//     }else{
-//         nytime.display = "none";
-//     }
-// };
 
 ko.applyBindings(new MapViewModel());
 
 //先定义地图会使用到的变量
-var map;
+var map,autocomplete;
 var markers = [];
 var locations = [
-    {title: 'Window of the World', location: {lat: 22.5347167, lng: 113.9734179}},
-    {title: 'OCT Bay', location: {lat: 22.5402908,lng: 113.9818588}},
-    {title: 'Coastal City', location: {lat: 22.516975,lng: 113.934716}},
-    {title: 'Shenzhen Bay Park', location: {lat: 22.5224158, lng: 114.0003285}},
-    {title: "Mount Yangtai", location: {lat: 22.6682145, lng: 113.9857292}},
-    {title: "Lianhuashan Park", location: {lat: 22.555043, lng: 114.058332}}
+    {title: '世界之窗', location: {lat: 22.5347167, lng: 113.9734179}},
+    {title: '欢乐谷', location: {lat: 22.5402908,lng: 113.9818588}},
+    {title: '海岸城', location: {lat: 22.516975,lng: 113.934716}},
+    {title: '深圳湾公园', location: {lat: 22.5224158, lng: 114.0003285}},
+    {title: "羊台山", location: {lat: 22.6682145, lng: 113.9857292}},
+    {title: "莲花山公园", location: {lat: 22.555043, lng: 114.058332}}
 ];
 //加载地图的函数
 var initMap = function() {
@@ -89,100 +67,45 @@ var initMap = function() {
             toggleBounce(this);
         });
     }
-    var input = document.getElementById('autocomplete');
-    var searchBox = new google.maps.places.SearchBox(input);
-    // Bias the SearchBox results towards current map's viewport.
-    map.addListener('bounds_changed', function () {
-        searchBox.setBounds(map.getBounds());
-    });
-    searchBox.addListener('places_changed', function () {
-        var places = searchBox.getPlaces();
-        if (places.length == 0) {
-            return;
-        }
-        // Clear out the old markers.
-        markers.forEach(function(marker) {
-            marker.setMap(null);
-        });
-       // markers = [];
-        for(var i = 0; i<markers.length;i++){
-            var marker = markers[i];
-            if(input.value !== marker.title){
-                alert("该地址不在查询范围内");
-                markers.forEach(function (marker) {
-                    marker.setMap(null);
-                });
-              //  markers = [];
-            }else{
-                markers.forEach(function (marker) {
-                    marker.setMap(null);
-                });
-                marker.setMap(map);
-                toggleBounce(marker);
-            }
 
-            //toggleBounce(marker);
-
-
-            // markers.push(new google.maps.Marker({
-            //     map: map,
-            //     //icon: icon,
-            //     title: place.name,
-            //     position: place.geometry.location
-            // }));
-            if (place.geometry.viewport) {
-                // Only geocodes have viewport.
-                bounds.union(place.geometry.viewport);
-            } else {
-                bounds.extend(place.geometry.location);
-            }
-        }
-
-        });
-        // For each place, get the icon, name and location.
-       /*
-        places.forEach(function (place) {
-            for(var i = 0; i<touristPlaces.length;i++){
-                var touristPlace = touristPlaces[i];
-                if (place !== touristPlace ) {
-                    console.log("该地址不在查询范围内");
-                    markers.forEach(function (marker) {
-                        marker.setMap(null);
-                    });
-                    markers = [];
-                }
-            }
-
-            // var icon = {
-            //     url: place.icon,
-            //     size: new google.maps.Size(71, 71),
-            //     origin: new google.maps.Point(0, 0),
-            //     anchor: new google.maps.Point(17, 34),
-            //     scaledSize: new google.maps.Size(25, 25)
-            // };
-            markers.push(new google.maps.Marker({
-                map: map,
-               //icon: icon,
-                title: place.name,
-                position: place.geometry.location
-            }));
-            if (place.geometry.viewport) {
-                // Only geocodes have viewport.
-                bounds.union(place.geometry.viewport);
-            } else {
-                bounds.extend(place.geometry.location);
-            }
-        });
-        */
         map.fitBounds(bounds);
-    };
+    autocomplete = new google.maps.places.Autocomplete(
+        (document.getElementById('autocomplete')), {
+            types: ['geocode'],
+            componentRestrictions: {country:  "CN"}
+        });
+    autocomplete.addListener('place_changed', fillInAddress);
+    var gobutton = $("#go");
+    gobutton.click(function() {
+        var place = document.getElementById('autocomplete').value;
+        handleAddress(place);
+    });
+};
 
-    // autocomplete = new google.maps.places.Autocomplete(
-    //     (document.getElementById('autocomplete')), {
-    //         types: touristPlaces,
-    //         componentRestrictions: {country:  "CN"}
-    //     });
-    // places = new google.maps.places.PlacesService(map);
+var handleAddress = function (place) {
+    if (place === undefined) {
+        return;
+    }
+    var targetMarker = null;
+    for(var i=0;i<markers.length;i++){
+        markers[i].setMap(null);
+        if (place == markers[i].title) {
+            targetMarker = markers[i];
+            targetMarker.setMap(map);
+            toggleBounce(targetMarker);
+            setTimeout(function() {
+                toggleBounce(targetMarker);
+            },1500);
+        }
+
+    }
+};
+
+var fillInAddress = function () {
+    var place = autocomplete.getPlace();
+    handleAddress(place.name);
+};
+
 
 var populateInfoWindow = function(marker,infoWindow){
     if(infoWindow.marker !== marker){
@@ -204,7 +127,7 @@ var toggleBounce = function (marker) {
 var wikiLink = function(marker){
     var addr = marker.title;
     var wikiHeader = $("#wiki-header");
-    var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' +addr+ '&format=json&callback=wikiCallback';
+    var wikiUrl = 'https://zh.wikipedia.org/w/api.php?action=opensearch&search=' +addr+ '&format=json&callback=wikiCallback';
     var wikiRequestTimeOut = setTimeout(function(){
         wikiHeader.append('<div> failed to get wikipedia sources</div>');
         toggleBounce(marker);
@@ -225,23 +148,8 @@ var wikiLink = function(marker){
 
 // Bias the autocomplete object to the user's geographical location,
 // as supplied by the browser's 'navigator.geolocation' object.
-/*
-var geolocate = function(){
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(function(position){
-            var geolocation = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-            var cicle = new google.maps.Circle({
-                center:geolocation,
-                radius:position.coords.accuracy
-            });
-            autocomplete.setBounds(circle.getBounds());
-        })
-    }
-};
-*/
+
+
 function geolocate() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
